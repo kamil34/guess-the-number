5efb53eb5f7f9a799814d64bc8f9cc940e55c490
Kamil <40839443+kamil34@users.noreply.github.com>
2026-01-18 12:56:28 +0100

    Add main.cpp with game menu options
    Just a simple main screen sketch for the app

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int option;
+    cout << "Wybierz numer aby przejść dalej\n";
+    cout << "1) Rozpocznij nową grę\n";
+    cout << "2) Sprawdź aktualne TOP5\n";
+    cin >> option;
+    cout << "Wybrano: " << option;
+}
